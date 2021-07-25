fa20d57af64bb53f1330f5a17a7fea5fdc7cbe86
Maureen-1998DEV <maureen.ougo@student.moringaschool.com>
2021-07-25 17:51:43 +0300

    modigied html and css file

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
- $(document).ready(function(){
-     $(".design").click (function(){
-        $(".designText").toggle();   
-   });     
-   $(".designText").click (function(){
-        $(".design").toggle();
-     });
-     $(".development").click(function(){
-         $(".developmentText").toggle();
-     });
-     $(".developmentText").click(function(){
-         $(".development").toggle();
-     });
-     $(".productManagement").click(function(){
-         $(".productManagementText").toggle();
-     });
-     $(".productManagement").click(function(){
-       $(".productManagement").toggle();
-     });
- });
+$(document).ready(function(){
+    $(".design").click (function(){
+       $(".designText").toggle();   
+  });     
+  $(".designText").click (function(){
+       $(".design").toggle();
+    });
+    $(".development").click(function(){
+        $(".developmentText").toggle();
+    });
+    $(".developmentText").click(function(){
+        $(".development").toggle();
+    });
+    $(".productManagement").click(function(){
+        $(".productManagementText").toggle();
+    });
+    $(".productManagement").click(function(){
+      $(".productManagement").toggle();
+    });
+});
